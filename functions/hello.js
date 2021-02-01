2acfd81ebc9c232aa819066c8b6249f37e60d43e
pedroSK <p.hlavacik@gmail.com>
2021-02-01 07:28:34 +0100

    basic-apifinished, CORS open api

--- functions/hello.js
+++ functions/hello.js
@@ -1,4 +1,4 @@
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, cb) => {
   return {
     statusCode: 200,
     body: "Hello Serverless Function in the Netlify World!😄",
